92cb5ea445242640f162d622f74360d11272d340
pillow12360 <ehdcks1256@naver.com>
2023-12-09 00:39:36 +0900

    Serive getQuestion 구현

--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -1,9 +1,14 @@
 package com.example.sbb.question;
 
+import com.example.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.zip.DataFormatException;
+
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
@@ -11,5 +16,16 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public  Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+
+        if (question.isPresent()){
+            return question.get();
+        }
+        else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
